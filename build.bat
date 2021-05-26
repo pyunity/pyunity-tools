51e48ea31d03875af74f12bbbe553f6d11b062d3
Ray Chen <tankimarshal2@gmail.com>
2021-05-26 17:08:06 +0100

    Remove unwanted files

--- build.bat
+++ build.bat
@@ -3,10 +3,10 @@
 py prepare.py
 py -m autopep8 -i -r --ignore E301,E302 pyunity_tools setup.py prepare.py cli.py
 py setup.py bdist_wheel sdist
-RMDIR /S /Q build\ pyunity_tools.egg-info/\
+RMDIR /S /Q build\ pyunity_tools.egg-info\
 IF NOT [%1] == [] (
 git add .
 git commit -m %1
 git push
 )
-pip install --upgrade dist/pyunity-tools-0.0.1-py3-none-any.whl
+pip install --upgrade dist/pyunity_tools-0.1.0-py3-none-any.whl
