1c146b47dafc7be8489576ca8268d0d8f1c33daf
Ray Chen <tankimarshal2@gmail.com>
2022-05-30 17:16:56 +0100

    Set appropriate zoom

--- pyunity_tools/apps/viewer/__init__.py
+++ pyunity_tools/apps/viewer/__init__.py
@@ -5,12 +5,17 @@ from .. import current_dir
 def main(file):
     if not os.path.isfile(file):
         raise click.BadParameter("File does not exist: " + repr(file))
-    if not file.endswith(".mesh"):
-        raise click.BadParameter("File is not in PyUnity Mesh format: " + repr(file))
-    
+    if not (file.endswith(".mesh") or file.endswith(".obj") or file.endswith(".stl")):
+        raise click.BadParameter("File is not in a readable mesh format: " + repr(file))
+
     from pyunity import Loader, SceneManager, MeshRenderer
+    funcs = {
+        "mesh": Loader.LoadMesh,
+        "obj": Loader.LoadObj,
+        "stl": Loader.LoadStl
+    }
     project = Loader.LoadProject(os.path.join(current_dir, "viewer", "viewer"))
-    scene = SceneManager.GetSceneByIndex(0)
-    mesh = Loader.LoadMesh(file)
+    scene = SceneManager.GetSceneByIndex(project.firstScene)
+    mesh = funcs[file.rsplit(".", 1)[-1]](file)
     scene.gameObjects[-1].GetComponent(MeshRenderer).mesh = mesh
     SceneManager.LoadScene(scene)
--- pyunity_tools/apps/viewer/viewer_source.py
+++ pyunity_tools/apps/viewer/viewer_source.py
@@ -1,13 +1,20 @@
 from pyunity import *
 
 class MouseViewer(Behaviour):
-    rot = Vector3.zero()
+    rot = Vector3(30, -30, 0)
     start = Vector3.zero()
     other = ShowInInspector(GameObject)
-    distance = ShowInInspector(int, 20)
+    zoom = ShowInInspector(float, 2)
+    minDist = ShowInInspector(float, 5, "Distance")
 
     def Start(self):
-        self.transform.position = Vector3(0, 0, -self.distance)
+        mesh = self.other.GetComponent(MeshRenderer).mesh
+        mag = mesh.max.abs().length
+        self.distance = max(mag * self.zoom, mag + self.minDist)
+
+        self.transform.localEulerAngles = self.rot
+        direction = self.transform.rotation.RotateVector(Vector3.forward()) * self.distance
+        self.transform.position = -direction
 
     def Update(self, dt):
         if Input.GetMouseDown(MouseCode.Left):
@@ -31,6 +38,8 @@ class ColorChanger(Behaviour):
         self.renderer.mat = Material(RGB(255, 0, 0))
 
 scene = SceneManager.AddScene("Scene")
+scene.gameObjects[1].transform.position = Vector3(10, 20, -30)
+scene.gameObjects[1].transform.LookAtPoint(Vector3(0, 0, 0))
 
 obj = GameObject("Object")
 renderer = obj.AddComponent(MeshRenderer)
