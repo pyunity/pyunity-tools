72bda66f86da23341a2695f1268bab621e01abd0
Ray Chen <tankimarshal2@gmail.com>
2022-05-30 12:56:29 +0100

    Use Mathf

--- pyunity_tools/apps/viewer/viewer_source.py
+++ pyunity_tools/apps/viewer/viewer_source.py
@@ -4,7 +4,7 @@ class MouseViewer(Behaviour):
     rot = Vector3.zero()
     start = Vector3.zero()
     other = ShowInInspector(GameObject)
-    distance = ShowInInspector(int, 10)
+    distance = ShowInInspector(int, 20)
 
     def Start(self):
         self.transform.position = Vector3(0, 0, -self.distance)
@@ -17,8 +17,8 @@ class MouseViewer(Behaviour):
             self.rot += Vector3(diff.y / Screen.height * 60,
                                 diff.x / Screen.width * 96,
                                 0)
-            self.rot.x = clamp(self.rot.x, -90, 90)
-            self.transform.localRotation.SetBackward(self.rot)
+            self.rot = self.rot.replace(0, Mathf.Clamp(self.rot.x, -90, 90))
+            self.transform.localEulerAngles = self.rot
 
             direction = self.transform.rotation.RotateVector(Vector3.forward()) * self.distance
             self.transform.position = -direction
